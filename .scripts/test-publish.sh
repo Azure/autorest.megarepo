9bc82e23990bd5111e58c4c1eb86d7305f2bfd55
Garrett Serack <garretts@microsoft.com>
2019-09-06 09:24:35 -0700

    still debugging package loading

--- .scripts/test-publish.sh
+++ .scripts/test-publish.sh
@@ -1,3 +1,6 @@
+rm -rf  ~/.local/share/verdaccio/storage/@autorest/
+rm -rf  ~/.local/share/verdaccio/storage/@azure-tools/
+rm -rf  ~/.local/share/verdaccio/storage/autorest/
 for file in common/temp/artifacts/packages/*.tgz 
 do
   npm publish $file --tag latest --access public --registry http://localhost:4873 || echo no-worries 
